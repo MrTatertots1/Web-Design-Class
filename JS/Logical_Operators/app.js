341c407f9ad92e286a6adfab1125a8f2ef58de8e
Aaron Work Desktop <aaron@abcgb.net>
2021-10-06 15:05:40 -0600

    working in js

---
+++ JS/Logical_Operators/app.js
@@ -0,0 +1,14 @@
+const age = parseInt(prompt("Enter an age"));
+
+if (age >= 0 && age < 5 || age >= 65) {
+    console.log("FREE");
+}
+else if (age >= 5 && age < 10) {
+    console.log("$10");
+}
+else if (age >= 10 && age < 65) {
+    console.log("$20");
+}
+else {
+    console.log("INVALID AGE");
+}
