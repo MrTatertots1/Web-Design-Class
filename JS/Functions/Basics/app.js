5cd2256ed427f35233dc4b133012cc07e28e253b
MrTatertots1 <mrtatertots1@outlook.com>
2021-10-07 19:10:08 -0600

    completed basic functions

---
+++ JS/Functions/Basics/app.js
@@ -0,0 +1,14 @@
+// function sing() {
+//     console.log("Do");
+//     console.log("Re");
+//     console.log("Me");
+// }
+
+function greet(firstName) {
+    console.log(`Hello ${firstName}`);
+}
+
+
+
+
+
