db4a30a5bca623bebb61035e9bf2f7492c72ab78
Aaron Anderson <mrtatertots1@outlook.com>
2022-01-07 22:05:48 -0700

    Added controllers and star reviews

--- YelpCamp/middleware.js
+++ YelpCamp/middleware.js
@@ -41,7 +41,7 @@ module.exports.validateReview = (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
     const review = await Review.findById(reviewId);
-    if (!review.author.equals(req.user_id)) {
+    if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
