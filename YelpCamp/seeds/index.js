55a4fc025f20371465e2ee9145322f74f0b6787e
MrTatertots1 <mrtatertots1@outlook.com>
2022-01-14 21:08:22 -0700

    Styling pages

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -13,7 +13,7 @@ const axios = require('axios');
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
-const url = 'https://api.unsplash.com/collections/429524/photos?client_id=2eEW2PZQpGVVSVAMd4sA4lHQ3tqHdsEia1RAdgo6x4M&page=1&per_page=50';
+const url = 'https://api.unsplash.com/collections/429524/photos?client_id=2eEW2PZQpGVVSVAMd4sA4lHQ3tqHdsEia1RAdgo6x4M&page=1&per_page=200';
 
 const seedDB = async () => {
     const allPhotos = await axios.get(url)
@@ -22,16 +22,23 @@ const seedDB = async () => {
         });
     await Campground.deleteMany({});
 
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 200; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             author: '61d4f82f09328c55a3c551e3',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
+            },
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, quos eligendi et corrupti dignissimos deserunt voluptate modi. Dolorum, animi fuga tenetur, ea corrupti, repudiandae totam aut pariatur eius libero earum.',
             price,
-            images: { url: allPhotos[i].urls.regular, filename: 'SEEDED IMAGE' }
+            images: { url: allPhotos[i % 30].urls.regular, filename: 'SEEDED IMAGE' }
         });
         await camp.save();
     }
